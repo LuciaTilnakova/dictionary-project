2a0474a26b57cf79a4b454c91506e460c01e24fd
Lucia Tilnakova <lucia.tilki@gmail.com>
2021-08-25 21:46:41 +0200

    Implemented search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <Dictionary />
+        <footer>
+          <small>
+            Coded by{" "}
+            <a
+              href="https://brave-morse-fe6f9e.netlify.app/"
+              className="mySite"
+            >
+              Lucia Tilnakova
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Search"
+          onChange={handleKeyword}
+        ></input>
+        <button className="btn btn-primary">Search</button>
+      </form>
+    </div>
+  );
+}
