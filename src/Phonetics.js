9adb8a33ad3707cb96edeb5c36057a905c3e8d09
Lucia Tilnakova <lucia.tilki@gmail.com>
2021-09-01 21:11:09 +0200

    Added Phonetics

---
+++ src/Phonetics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  return (
+    <div className="Phonetics">
+      <a href={props.phonetic.audio} target="noreferrer">
+        Listen
+      </a>
+      <br />/{props.phonetic.text}/
+    </div>
+  );
+}
