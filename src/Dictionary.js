eb37fb8595643b930306591ffd21c1492fadac6e
Lucia Tilnakova <lucia.tilki@gmail.com>
2021-09-05 21:03:17 +0200

    Added photos API, improved styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
+
+  function handlepexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
   function handleResponse(response) {
     console.log(response.data[0]);
@@ -22,6 +28,11 @@ export default function Dictionary() {
     let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiURL).then(handleResponse);
     //documentation:https://dictionaryapi.dev/
+
+    let pexelsApiKey = `563492ad6f91700001000001c625ebdda13c4b34bd2422b81ee0fa7f`;
+    let pexelsApiURL = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiURL, { headers: headers }).then(handlepexelsResponse);
   }
 
   return (
@@ -38,6 +49,7 @@ export default function Dictionary() {
         <div className="hint">search for: nature, wine, summer, yoga ...</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
